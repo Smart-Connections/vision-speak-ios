b9baaf304e4467ba3917b4abb081fc6292d49cac
Shion Maruko <conquerx1@gmail.com>
2023-07-23 16:49:01 +0900

    Initial Commit

---
+++ EnglishCamera/EnglishCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishCameraApp.swift
+//  EnglishCamera
+//
+//  Created by maruko_shion_default on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EnglishCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
